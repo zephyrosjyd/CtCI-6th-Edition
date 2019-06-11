5a56134cbc6e0496abbda46402ca0b591523abbc
zephyrosjyd <zephyrosjyd@gmail.com>
2019-06-12 02:05:40 +0900

    update chap2-4

--- MyExercises/Python/Chapter2/4_Partition.py
+++ MyExercises/Python/Chapter2/4_Partition.py
@@ -3,13 +3,16 @@ from LinkedList import LinkedList
 
 def partition(ll, x):
     current = ll.tail = ll.head
+    print(current, ll.tail, ll.head)
 
     while current:
         nextNode = current.next
         current.next = None
         if current.value < x:
+            print(current.value, current.next, ll.head)
             current.next = ll.head
             ll.head = current
+            print(current.value, current.next, ll.head, current.next.next)
         else:
             ll.tail.next = current
             ll.tail = current
@@ -19,9 +22,12 @@ def partition(ll, x):
     if ll.tail.next is not None:
         ll.tail.next = None
 
+#def partition2(ll, x):
+#    curr = 
+
 
 ll = LinkedList()
-ll.generate(10, 0, 99)
+ll.generate(8, 1, 10)
 print(ll)
-partition(ll, ll.head.value)
+partition(ll, 5)
 print(ll)
